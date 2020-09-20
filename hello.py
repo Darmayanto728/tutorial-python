3d4e85caf8156a39031aa5f1fc823d4c63d27a8e
darmayanto728 <darmaputra728@gmail.com>
2020-09-20 13:01:00 +0700

    string dan konversi

--- hello.py
+++ hello.py
@@ -7,5 +7,18 @@ def rumus_abc (a, b, c):
     print (x_1)
     print (x_2)
 
+    print("akarnya adalah " + str(x_1)+ " dan " + str(x_2) + ".")
+    
 rumus_abc(1,2,1)
 rumus_abc(2,-5,-3)
+
+nama_depan = "Darma"
+nama_tengah = "Yanto"
+nama_belakang = "Putra"
+nama_lengkap = nama_depan + " " + nama_tengah+" "+ nama_belakang
+print (nama_lengkap)
+
+a1 = 10
+a2 = 3
+print (a1*a2)
+print (oct(a1*a2))
